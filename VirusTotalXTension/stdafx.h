e7302acc1b79a3db802b064e24ff2c4ce926a182
Ian <Ian@IAN-WORK>
2016-09-26 18:05:00 -0400

    Add project files.

---
+++ VirusTotalXTension/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <Windows.h>
+
+
+
+// TODO: reference additional headers your program requires here
+#include "RLXTension.h"
+#include <WinInet.h>
+#include <strsafe.h>
+#include <stdlib.h>
+#include <malloc.h>
